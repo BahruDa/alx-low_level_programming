8347ff4536a7b5a9eb59e035e1ebb3ec54bd9e3a
BahruDa <bahrudari68@gmail.com>
2022-11-28 23:01:22 +0300

    .

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,76 +1,41 @@
 #include "lists.h"
 
 /**
- * detect_loop - Function that detects if is a loop
- * @h: Head of the linked list
- * Return: Number of length if is true or 0 if is false
+ * find_listint_loop - Finds the loop contained in
+ *                     a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: If there is no loop - NULL.
+ *         Otherwise - the address of the node where the loop starts.
  */
-int detect_loop(listint_t *h)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow, *fast;
-	int i;
+	listint_t *tortoise, *hare;
 
-	slow = h, fast = h;
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-	for (i = 1; slow && fast && fast->next; i++)
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		slow = slow->next, fast = fast->next->next;
-
-		if (slow == fast)
-			return (i);
-	}
-
-	return (0);
-}
-
-/**
- * remove_loop - Function that removes loop in a linked list
- * @h: Header of the linked list
- * @n: lenth of the loop
- */
-void remove_loop(listint_t **h, int n)
-{
-	listint_t *ptr1, *ptr2;
-
-	ptr1 = *h, ptr2 = *h;
-
-	for (; n > 0; n--)
-		ptr1 = ptr1->next;
-
-	while (1)
-	{
-		ptr1 = ptr1->next, ptr2 = ptr2->next;
-
-		if (ptr1->next == ptr2->next)
+		if (tortoise == hare)
 		{
-			ptr1->next = NULL;
-			return;
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			return (tortoise);
 		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
-}
 
-/**
- * free_listint_safe - Function that frees a linked list
- * @h: Head of the linked list
- * Return: The size of the list that was free
- */
-size_t free_listint_safe(listint_t **h)
-{
-	listint_t *t;
-	int n;
-
-	if (h == NULL || *h == NULL)
-		printf("0\n(nil)\n"), exit(98);
-
-	n = detect_loop(*h);
-
-	if (n != 0)
-		remove_loop(h, n);
-
-	for (n = 0; *h; n++)
-		t = *h, *h = (*h)->next, free(t);
-
-	*h = NULL;
-
-	return (n);
+	return (NULL);
 }
