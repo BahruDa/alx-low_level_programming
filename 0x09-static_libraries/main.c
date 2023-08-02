052a9843f2a9c561a32ef41e6fdc158d1e9f2a8b
BahruDa <bahrudari68@gmail.com>
2023-08-02 18:19:34 +0300

    hot fix

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -3,9 +3,5 @@
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-<<<<<<< HEAD
-     return (0);
-=======
     return (0);
->>>>>>> d7a5346a3776e683ef8e65865234c0e1688f4020
 }
